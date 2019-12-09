86d30d78bc14227f01eed154cb54c6be86589918
mgallagher56 <marc.gallagherreid11@bathspa.ac.uk>
2019-12-09 16:57:14 +0000

    added validation for favourite character input

---
+++ src/validation.js
@@ -0,0 +1,15 @@
+
+charcterArray = ['Baby Mario', 'Baby Peach', 'Toad',
+    'Koopa Troopa', 'Mario', 'Luigi', 'Peach',
+    'Yoshi', 'Wario', 'Waluigi', 'Donkey Kong',
+    'Bowser', 'Baby Daisy', 'Baby Luigi', 'Birdo',
+    'Bowser Junior', 'Daisy', 'Diddy Kong', 'Dry Bones',
+    'Dry Bowser', 'Funky King', 'King Boo',
+    'Mii', 'Rosalina', 'Toadette'
+];
+
+function validateUserData (req) {
+
+    if (charcterArray.includes(req.body.favRacer)){
+    }
+}
