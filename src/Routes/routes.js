5a89db8458c2685dc458b2b606db7310047211d1
git <lukechaston@gmail.com>
2019-12-10 10:32:58 +0000

    made new route.

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -5,7 +5,11 @@ const  UserController = require ('../Controllers/UserController');
 function routes (app) {
     app.post('/api/user', jsonParser, UserController.addUser, function (req, res) {
         res.send('Creating new user');
-    });
+    })
+
+    app.get('/api/allusers', jsonParser, UserController.getAllUsers,function (req, res) {
+        res.send('Here are all the users')
+    })
 }
 
 module.exports = routes;
