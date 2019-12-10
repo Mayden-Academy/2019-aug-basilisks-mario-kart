e6e055866604a83c9ec25f8caf33aabf57711d5d
git <lukechaston@gmail.com>
2019-12-10 11:36:51 +0000

    added service for getting by id function.

--- src/Services/UserService.js
+++ src/Services/UserService.js
@@ -13,6 +13,13 @@ var getUsers = function (db, callback) {
     })
 };
 
+var getUserById = function (id, db, callback) {
+    var collection = db.collection('mario-kart-users');
+    collection.find({"_id": id }).toArray(function (id, err, documents) {
+        callback(documents)
+    })
+};
 
 module.exports.addUser = addUser;
 module.exports.getUsers = getUsers;
+module.exports.getUserById = getUserById;
