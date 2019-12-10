07e6f6b13b3354fc8ea6ff21d0b433eb8a8cc4fd
mgallagher56 <marc.gallagherreid11@bathspa.ac.uk>
2019-12-10 09:35:12 +0000

    updated validation function

--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -1,18 +1,15 @@
 const UserService = require('../Services/UserService');
 const DbService = require('../Services/DbService');
+const validateUserData = require ('../validateUserData');
 
 function addUser (req, res) {
-    let newUser = {
-        name: req.body.name,
-            favRacer: req.body.favRacer,
-            cohort: req.body.cohort
-    };
+    let newUser = validateUserData(req);
     DbService.connectToDB(((db) => {
         UserService.addUser(db, newUser, (result) => {
             if (result.insertedCount ===1) {
-                res.json({success: true, msg: 'added new user to db', data: []})
+                res.json({success: true, msg: 'added new user to db', data: newUser})
             } else {
-                res.json({success: false, msg: 'User not added to db', data: []})
+                res.json({success: false, msg: 'User not added to db', data: newUser})
             }
         })
     }))
---
+++ src/validateUserData.js
@@ -0,0 +1,26 @@
+
+charcterArray = ['Baby Mario', 'Baby Peach', 'Toad',
+    'Koopa Troopa', 'Mario', 'Luigi', 'Peach',
+    'Yoshi', 'Wario', 'Waluigi', 'Donkey Kong',
+    'Bowser', 'Baby Daisy', 'Baby Luigi', 'Birdo',
+    'Bowser Junior', 'Daisy', 'Diddy Kong', 'Dry Bones',
+    'Dry Bowser', 'Funky King', 'King Boo',
+    'Mii', 'Rosalina', 'Toadette'
+];
+
+function htmlEntities(str) {
+    return String(str).replace(/&/g, '').replace(/</g, '').replace(/>/g, '').replace(/"/g, '').replace(/[0-9]/gm, '' );
+}
+
+function validateUserData (req) {
+    let usersName = htmlEntities(req.body.name);
+    let character = htmlEntities(req.body.favRacer);
+    let userCohort = htmlEntities(req.body.cohort);
+    if (charcterArray.includes(character)){
+        return {name: usersName,
+        favRacer: character,
+        Cohort: userCohort}
+    }
+}
+
+module.exports.validateUserData = validateUserData;
