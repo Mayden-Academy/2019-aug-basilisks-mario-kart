06d31b23f80dde94892a779872a7eeaf6ab76db1
mgallagher56 <marc.gallagherreid11@bathspa.ac.uk>
2019-12-11 10:38:39 +0000

    added doc blocks to functions

--- src/validation.js
+++ src/validation.js
@@ -8,10 +8,27 @@ charcterArray = ['Baby Mario', 'Baby Peach', 'Toad',
     'Mii', 'Rosalina', 'Toadette'
 ];
 
+/**
+ * Takes a string and removes any character that is not a letter
+ *
+ * @param str string of characters
+ *
+ * @return string string with only lowercase and uppercase letters a-z
+ *
+ */
 function htmlEntities(str) {
     return String(str).replace(/[^a-zA-Z ]/g, '');
 }
 
+
+/**
+ * Takes a http request and validates and sanitises data from body. Returns validated object of user data
+ *
+ * @param req HTTP Request
+ *
+ * @return object of validated user data
+ *
+ */
 function validateUserData (req) {
     let usersName = htmlEntities(req.body.name);
     let character = htmlEntities(req.body.favRacer);
