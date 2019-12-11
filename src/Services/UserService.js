0099327e17c21a1e715e12325db94caa9e51d9e7
mgallagher56 <marc.gallagherreid11@bathspa.ac.uk>
2019-12-11 11:07:10 +0000

    added docblocks to functions

--- src/Services/UserService.js
+++ src/Services/UserService.js
@@ -1,10 +1,27 @@
+/**
+ * Adds a new user to the db collection 'mario-kart-users'
+ *
+ * @param db database connection
+ *
+ * @param newUser object container new user data
+ *
+ * @param callback function
+ *
+ */
 addUser = (db, newUser, callback ) => {
     db.collection('mario-kart-users').insertOne(newUser, (result) => {
         callback(result);
-
     });
 };
 
+/**
+ * Gets all users from db collection 'mario-kart-users'
+ *
+ * @param db database connection
+ *
+ * @param callback function
+ *
+ */
 getUsers = (db, callback) => {
     db.collection('mario-kart-users').find().toArray((err, documents) => {
         if (err)
@@ -13,6 +30,16 @@ getUsers = (db, callback) => {
     })
 };
 
+/**
+ * Gets a single user from db collection 'mario-kart-users'
+ *
+ * @param db database connection
+ *
+ * @param id  mongodb ObjectId of user
+ *
+ * @param callback function
+ *
+ */
 getUserById = (db, id, callback) => {
     db.collection("mario-kart-users").findOne({"_id": id}, (err, documents) => {
         if (err)
