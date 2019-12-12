17a345002bdf75cf4ae4617ce6fb24508a8717a2
mgallagher56 <marc.gallagherreid11@bathspa.ac.uk>
2019-12-12 13:37:59 +0000

    added validation and error message to function

--- src/Controllers/TrackController.js
+++ src/Controllers/TrackController.js
@@ -1,17 +1,25 @@
 const TrackService = require('../Services/TrackService');
 const DbService = require('../Services/DbService');
-const validation = require('../Services/ValidationService');
+const validation = require('../validation');
 const averageFinishPositionService = require('../Services/AverageFinishPositionService');
 
 
 function getTrackResults(req, res) {
-    let trackName = req.params.name
-    DbService.connectToDB(function (db){
-        TrackService.getTrackResults(db, trackName, function (results) {
-            let thing = averageFinishPositionService.averageFinishPosition(results, trackName)
-            res.json(thing)
+    let trackName = req.params.track;
+    trackName = validation.validateTrack(trackName);
+    if (trackName) {
+        DbService.connectToDB((db) => {
+            TrackService.getTrackResults(db, trackName, (documents) => {
+                let results = averageFinishPositionService.averageFinishPosition(documents, trackName);
+                res.json(results)
+            })
         })
-    })
+    } else {
+        res.json({
+            msg: 'Could not get results. Please ensure the track is valid. Check API docs for track list',
+        })
+    }
 }
+
 
 module.exports.getTrackResults = getTrackResults;
